4a62878d868534d9790d4d9e1ba31c20c5b235af
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-21 16:07:14 -0300

    Finalizando GrausFC

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double N1, resp;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("GrausFC");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("--------");
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Digite uma temperatura: ");
+            
+            N1 = Convert.ToDouble(Console.ReadLine());
+            
+            resp = ( N1 - 32  ) /  1.8;
+            
+            Console.WriteLine($"°F = {N1}");
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"{N1}°F ");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("equivale a: ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"{resp.ToString("f")}°F");
+
+            Console.ResetColor();
+        }
+    }
+}
